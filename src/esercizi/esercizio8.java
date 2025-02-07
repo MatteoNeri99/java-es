ad09af49418a82979b5e9f5a3dce037cc9eea406
MatteoNeri99 <matteolavoro99@gmail.com>
2025-02-07 10:57:54 +0100

    es 8 IF

---
+++ src/esercizi/esercizio8.java
@@ -0,0 +1,16 @@
+package esercizi;
+
+public class esercizio8 {
+
+    public static void main(String[] args) {
+
+        //Scrivi un programma Java che calcola il prezzo scontato di un prodotto in base all'importo e al tasso di sconto.
+
+        double prezzo = 13.0;
+        double sconto = 20.0;
+        double prezzoScontato = prezzo -  (prezzo  * (sconto /100))  ;
+        System.out.println("il prezzo scontato è : " + prezzoScontato);
+
+    }
+}
+
