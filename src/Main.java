89da2b4d39bb87ff4b7366a0c7f3e019fd35ba4f
MatteoNeri99 <matteolavoro99@gmail.com>
2025-02-07 08:45:18 +0100

    es 2 IF

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int numero = 0;
 
-        if (numero > 0){
+       // Scrivi un programma Java che verifica se un numero intero è positivo, negativo o zero.
 
-            System.out.println("numero è positivo");
+        int numero = 10;
 
-        }else if (numero < 0 ){
+        if (numero % 2 == 0){
 
-            System.out.println("numero è negativo");
+            System.out.println("il numero è pari");
+        }else{
 
-        }else {
-
-            System.out.println("il numero è pari a 0");
+            System.out.println("il numero è dispari");
         }
+
+
+
 
     }
 }
