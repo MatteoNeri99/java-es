b4a4bee64d4818a0fe11e47adfd88375b3134b2c
MatteoNeri99 <matteolavoro99@gmail.com>
2025-02-07 09:10:29 +0100

    es 3 IF

---
+++ src/esercizi/tre/esercizio3.java
@@ -0,0 +1,20 @@
+package esercizi.tre;
+
+public class esercizio3 {
+
+    public static void main(String[] args) {
+
+        //Scrivi un programma Java che verifica se un carattere è una lettera maiuscola o una lettera minuscola.
+
+        char a = 'a';
+
+        if (Character.isUpperCase(a)){
+
+            System.out.println("il carattere è maiuscolo");
+        }else{
+
+            System.out.println("il carattere è minuscolo");
+        }
+
+    }
+}
