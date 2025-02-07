aea2e92b80a4726abc9c889f6b5dca6ef6895afd
MatteoNeri99 <matteolavoro99@gmail.com>
2025-02-07 08:41:57 +0100

    es 1 IF

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int numero = 0;
+
+        if (numero > 0){
+
+            System.out.println("numero è positivo");
+
+        }else if (numero < 0 ){
+
+            System.out.println("numero è negativo");
+
+        }else {
+
+            System.out.println("il numero è pari a 0");
+        }
 
     }
 }
