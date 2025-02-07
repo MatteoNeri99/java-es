5ba3c62c6d5afbb4c7f4bde295cf16a975137595
MatteoNeri99 <matteolavoro99@gmail.com>
2025-02-07 10:23:52 +0100

    es 6 IF

---
+++ src/esercizi/esercizio6.java
@@ -0,0 +1,23 @@
+package esercizi;
+
+public class esercizio6 {
+
+
+    public static void main(String[] args) {
+
+        //Scrivi un programma Java che verifica se una stringa è vuota o null.
+
+        String stringa = "";
+        if (stringa == null || stringa.isEmpty()) {
+            System.out.println("La stringa è vuota o null.");
+        } else {
+            System.out.println("La stringa non è vuota o null.");
+        }
+
+
+
+
+    }
+
+
+}
