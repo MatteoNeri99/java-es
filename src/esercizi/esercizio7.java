02f7b668824ee731c5a3608147cead3d5e53a8c2
MatteoNeri99 <matteolavoro99@gmail.com>
2025-02-07 10:40:11 +0100

    es 7 IF

---
+++ src/esercizi/esercizio7.java
@@ -0,0 +1,27 @@
+package esercizi;
+
+public class esercizio7 {
+
+    public static void main(String[] args) {
+
+        //Scrivi un programma Java che verifica se un carattere è una vocale o una consonante
+
+        char a ='c';
+
+        if (a == 'a'  || a == 'e' || a == 'i' || a == 'o' || a == 'u'){
+
+            System.out.println("il carattere è una vocale");
+
+        }else{
+
+            System.out.println("il carattere è una consonante");
+        }
+
+
+
+
+
+
+
+    }
+}
