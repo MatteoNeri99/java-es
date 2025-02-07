934f53bc23cb89ea570d894aaf8d4cc54dded85f
MatteoNeri99 <matteolavoro99@gmail.com>
2025-02-07 09:33:22 +0100

    es 4 IF

---
+++ src/esercizi/esercizio4IF.java
@@ -0,0 +1,21 @@
+package esercizi;
+
+public class esercizio4IF {
+
+    public static void main(String[] args) {
+
+        //Scrivi un programma Java che verifica se un numero è positivo e pari.
+
+        int numero = 5;
+
+        if (numero > 0 && numero % 2 == 0){
+
+            System.out.println("il numero e sia pari che positivo");
+        }else{
+
+            System.out.println("il numero non è positivo,pari o entrambi");
+        }
+
+
+    }
+}
